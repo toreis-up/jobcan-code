a81f00145a57157bf3faf73ac8b06ca6bb71d0b8
tores-up <toreis@reisan.dev>
2024-02-29 23:58:42 +0900

    init: 1FA login

---
+++ README.md
@@ -0,0 +1,31 @@
+# [WIP]jobcan-code README
+
+!!! THIS PROJECT IS WIP !!!
+
+This is the README for your extension "jobcan-code".
+
+`I wonder touch with VSCode...`
+
+## Features
+
+- [x] Login (Classic)
+- [ ] Login (2FA)
+- [ ] Touch
+  - [ ] Indicate current working status
+  - [ ] Switch automatically as yakin mode
+- [ ] Check working time
+
+## Requirements
+
+```
+node >= 18
+npm >= 9.5.1
+```
+
+## Extension Settings
+
+None
+
+In "Command Palette", you can set username and password.
+
+## Known Issues
---
+++ src/extension.ts
@@ -0,0 +1,126 @@
+import * as vscode from "vscode";
+import type { SecretStorage } from "vscode";
+import { parse } from "node-html-parser";
+import { CookieJar } from 'tough-cookie';
+import { CookieAgent } from 'http-cookie-agent/undici';
+import { fetch } from "undici";
+
+const jar = new CookieJar();
+const agent = new CookieAgent({ cookies: { jar } });
+
+const getMetaValue = async (htmlString: string, tagName: string) => {
+  const html = await parse(htmlString);
+  const htmlElement = html.querySelectorAll("meta").find((val) => {
+    return val.attributes.name === tagName;
+  });
+  return htmlElement?.attributes.content || "";
+};
+
+const getPageTitle = async (htmlString: string) => {
+  const html = await parse(htmlString);
+  return html.querySelectorAll("title")[0]?.text || "";
+};
+
+export function activate(context: vscode.ExtensionContext) {
+  const secretStorage: SecretStorage = context.secrets;
+
+  let jobcanTouch = vscode.commands.registerCommand(
+    "jobcan-code.jobcan-touch",
+    () => {
+      vscode.window.showInformationMessage("Hello World from jobcan-code! (Not implemented)");
+    }
+  );
+
+  const fetchCSRF = async () => {
+    await fetch("https://id.jobcan.jp/users/sign_in", { dispatcher: agent })
+      .then(async (res) => {
+        const htmlString = await res.text();
+        let csrfToken = await getMetaValue(htmlString, "csrf-token");
+        await secretStorage.store("jobcan-csrf-token", csrfToken);
+      })
+      .catch(async (e: Error) => {
+        await vscode.window.showErrorMessage(e.message);
+      });
+  };
+
+  let jobcanLogin = vscode.commands.registerCommand(
+    "jobcan-code.jobcan-login",
+    async () => {
+			const username = await secretStorage.get("jobcan-username");
+			const password = await secretStorage.get("jobcan-password");
+			if (!username || !password) {
+				vscode.window.showErrorMessage("ユーザー名もしくはパスワードが設定されていません。");
+				return;
+			}
+      await fetchCSRF().then(async () => {
+
+			const formData = new FormData();
+			formData.append("authenticity_token", await secretStorage.get("jobcan-csrf-token"));
+			formData.append("user[email]", await secretStorage.get("jobcan-username"));
+			formData.append("user[client_code]", "");
+			formData.append("user[password]", await secretStorage.get("jobcan-password"));
+			formData.append("save_sign_in_information", "true");
+			formData.append("app_key", "atd");
+			formData.append("commit", "ログイン");
+
+      await fetch("https://id.jobcan.jp/users/sign_in", {
+        method: "POST",
+        body: formData,
+        dispatcher: agent
+      })
+			.then(async (res) => {
+				const resText = await res.text();
+        let csrfToken = await getMetaValue(resText, "csrf-token");
+        await secretStorage.store("jobcan-csrf-token", csrfToken);
+        return (await getPageTitle(resText)).startsWith("JOBCAN MyPage:");
+			})
+			.then(async (isLoggedIn) => {
+				if (isLoggedIn) {
+					await vscode.window.showInformationMessage("Logged In!");
+					return;
+				}
+        await vscode.window.showInformationMessage("Login Failed. Check your username or password.");
+        // TODO: 2FA
+			})
+			.catch(async (e: Error) => {
+				await vscode.window.showErrorMessage(e.message);
+			});
+
+			});
+    }
+  );
+
+  const setUsername = vscode.commands.registerCommand(
+    "jobcan-code.jobcan-set-username",
+    async () => {
+      const username = await vscode.window.showInputBox({
+        title: "メールアドレスまたはスタッフコードを入力してください",
+        placeHolder: "test@example.net",
+      });
+      await secretStorage.store("jobcan-username", username || "");
+      await vscode.window.showInformationMessage("Set jobcan's username!");
+    }
+  );
+
+  const setPassword = vscode.commands.registerCommand(
+    "jobcan-code.jobcan-set-password",
+    async () => {
+      const password = await vscode.window.showInputBox({
+        title: "パスワードを入力してください",
+        password: true,
+      });
+      await secretStorage.store("jobcan-password", password || "");
+      await vscode.window.showInformationMessage("Set jobcan's password!");
+    }
+  );
+
+  context.subscriptions.push(
+    jobcanTouch,
+    jobcanLogin,
+    setPassword,
+    setUsername,
+  );
+}
+
+// This method is called when your extension is deactivated
+export function deactivate(context: vscode.ExtensionContext) {}
